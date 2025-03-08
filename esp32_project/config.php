2968761822ba7be91d83593aad05a619aade4773
Rothezee <rothalan83@gmail.com>
2025-03-08 12:28:40 -0300

    Add files via upload

---
+++ esp32_project/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "39090169";
+$dbname = "esp32_project";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Establecer el modo de error de PDO a excepción
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
